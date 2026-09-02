9402c86f3220550091ccc197568d4f781865b95e
Davide Innocenti <d.innocenti@chemitec.it>
2026-09-02 15:47:48 +0200

    Aggiunto services

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 print("Configuro encoding...")
 if sys.platform == "win32":
